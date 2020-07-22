0c6846b1c2322a546852151dddb69f02ba136473
YOGA <YOGA@DESKTOP-KEIVQ3I>
2020-07-22 22:18:19 +0530

    Completed project

---
+++ Credit Card Bill Generator/src/com/wipro/ccbill/exception/InputValidationException.java
@@ -0,0 +1,14 @@
+package com.wipro.ccbill.exception;
+
+public class InputValidationException extends Exception {
+
+	public String toString()
+	{
+		return "Invalid Input Data";
+	}
+	public InputValidationException(String str)
+	{
+		super(str);
+		System.out.println(str);
+	}
+}
